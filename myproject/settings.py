da29c2957e4b0683f590f5beb3876a8f21a43b31
anzelikagudkova <kkeerrll137@gmail.com>
2023-10-04 09:10:01 +0300

    New

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store',
+        'USER': 'postgres',
+        'PASSWORD': 'lika2244',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
